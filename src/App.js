a661910d11b7d19797a4f0a0ec1e0a2db3d38428
toMilanesi <tomi.milanesi@gmail.com>
2022-07-25 18:45:53 -0300

    Added Header component a basic styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from './components/Main/Main';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path='/' element={<Main />} />
       </Routes>
