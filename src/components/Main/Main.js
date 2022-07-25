92e274b671dae002654f98d3df39a7f506712d15
toMilanesi <tomi.milanesi@gmail.com>
2022-07-25 19:28:30 -0300

    Added navigation adn styles

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import styles from './Main.module.scss';
 
 function Main() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <main className={styles.container}>
+      <nav className={styles.nav}>
+        <ul className={styles.ulCont}>
+          <li><NavLink to="/">Styles</NavLink></li>
+          <li><NavLink to="/">Hops</NavLink></li>
+          <li><NavLink to="/">Yeasts</NavLink></li>
+        </ul>
+      </nav>
+    </main>
   )
 }
 
